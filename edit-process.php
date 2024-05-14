4c011228f58eb0ac4fc902b104905caa2ef82343
Rosyid Eko Nugroho <90685439+Rosyieed@users.noreply.github.com>
2024-05-14 18:10:59 +0700

    Fix: Edit Article

--- edit-process.php
+++ edit-process.php
@@ -1,51 +1,61 @@
 <?php
 include 'koneksi.php';
+
 if (isset($_POST['edit'])) {
     $title = $_POST['title'];
     $id = $_POST['id'];
     $content = $_POST['content'];
     $category = $_POST['category'];
-    $thumbnail = $_POST['thumbnail'];
 
-    $extension_allowed = array('png', 'jpg');
-    $name = $_FILES['thumbnail']['name'];
-    $x = explode('.', $name);
-    $extension = strtolower(end($x));
-    $size = $_FILES['thumbnail']['size'];
-    $file_tmp = $_FILES['thumbnail']['tmp_name'];
-    if (in_array($extension, $extension_allowed) === true) {
-        if ($ukuran < 1044070) {
-            move_uploaded_file($file_tmp, 'images/' . $name);
-            $query = mysqli_query($koneksi, "UPDATE articles SET
-title='$title', content='$content', category='$category',
-thumbnail='$name' WHERE id='$id'");
+    if (isset($_FILES['thumbnail']) && $_FILES['thumbnail']['error'] == UPLOAD_ERR_OK) {
+        $thumbnail = $_FILES['thumbnail'];
 
-            if ($query) {
-                $message = "Data berhasil diubah";
-                $message = urlencode($message);
-                header("Location:index.php?message={$message}");
+        $extension_allowed = array('png', 'jpg');
+        $name = $thumbnail['name'];
+        $x = explode('.', $name);
+        $extension = strtolower(end($x));
+        $size = $thumbnail['size'];
+        $file_tmp = $thumbnail['tmp_name'];
+
+        if (in_array($extension, $extension_allowed) === true) {
+            if ($size < 1044070) {
+                move_uploaded_file($file_tmp, 'images/' . $name);
+                $query = mysqli_query($koneksi, "UPDATE articles SET title='$title', content='$content', category='$category', thumbnail='$name' WHERE id='$id'");
+                if ($query) {
+                    $message = "Data berhasil diubah";
+                    $message = urlencode($message);
+                    header("Location: index.php?message={$message}");
+                    exit();
+                } else {
+                    $message = "Data gagal diubah";
+                    $message = urlencode($message);
+                    header("Location: add.php?message={$message}");
+                    exit();
+                }
             } else {
-                $message = "Data gagal diubah";
+                $message = "Ukuran File Terlalu Besar";
                 $message = urlencode($message);
-                header("Location:add.php?message={$message}");
+                header("Location: add.php?message={$message}");
+                exit();
             }
         } else {
-            $message = "Ukuran File Terlalu Besar";
+            $message = "Extension tidak diperbolehkan";
             $message = urlencode($message);
-            header("Location:add.php?message={$message}");
+            header("Location: add.php?message={$message}");
+            exit();
         }
     } else {
-        $query = mysqli_query($koneksi, "UPDATE articles SET
-title='$title', content='$content', category='$category' WHERE
-id='$id'");
+        $query = mysqli_query($koneksi, "UPDATE articles SET title='$title', content='$content', category='$category' WHERE id='$id'");
         if ($query) {
             $message = "Data berhasil diubah";
             $message = urlencode($message);
-            header("Location:index.php?message={$message}");
+            header("Location: index.php?message={$message}");
+            exit();
         } else {
             $message = "Data gagal diubah";
             $message = urlencode($message);
-            header("Location:add.php?message={$message}");
+            header("Location: add.php?message={$message}");
+            exit();
         }
     }
 }
